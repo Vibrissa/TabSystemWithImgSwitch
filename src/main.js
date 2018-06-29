a9493ca8dca62e4adf6255a61f83cf464142daf5
Vibrissa <zzozzozz15@gmail.com>
2018-06-29 14:23:15 +0800

    - change to new cli

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import App from "./App";
+import Vue from 'vue';
+import App from './App.vue';
 import store from "./store";
 import Element from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+
 Vue.config.productionTip = false;
 Vue.use(Element);
-/* eslint-disable no-new */
 new Vue({
-  el: "#app",
-  store,
-  components: { App },
-  template: "<App/>"
-});
+  render: h => h(App),
+  store
+}).$mount('#app')
